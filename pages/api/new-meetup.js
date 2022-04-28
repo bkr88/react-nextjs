ea4824844b1f2a61b5d7c551d3db851f5e8ef8ed
antalbokor <antalbokor@gmail.com>
2022-04-28 17:05:22 +0200

    Pre mongodb install

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,9 @@
+const handler = (req, res) => {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const { title, image, description, addess } = data;
+  }
+};
+
+export default handler;
